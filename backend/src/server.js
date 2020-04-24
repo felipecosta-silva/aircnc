2f08c4fc1c4d5aa0f7ec84230283d9902f557d54
Felipe Costa <felipecostati@gmail.com>
2020-04-24 04:05:31 -0300

    adição do CORS no server do backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 
@@ -10,6 +12,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-2lyu3.mongodb.net/
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
