9b0ddce6ecee50afd85c6570f5e22d2ac309bf54
Felipe Costa <felipecostati@gmail.com>
2020-04-26 04:29:53 -0300

    atualização nos arquivos server.js e spot.js metodo para fazer o upload dasimagens (thumbnail)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -14,6 +15,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-2lyu3.mongodb.net/
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
